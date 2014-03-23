bd90f10b16b064f0fcd5bae220404f986b86f1d4
Alex Nichol <unixpickle@gmail.com>
2014-03-22 21:06:19 -0400

    implemented timer interrupt

--- test/env/interrupts.c
+++ test/env/interrupts.c
@@ -1,3 +1,17 @@
+#include "interrupts.h"
+#include "threading.h"
+#include "context.h"
+#include <anscheduler/loop.h>
+
 void antest_handle_timer_interrupt() {
-  // here, we need to switch into a nice CPU context and call the runloop.
+  thread_t * th = anscheduler_cpu_get_thread();
+  if (th) {
+    bool retVal = anscheduler_save_return_state(th);
+    if (retVal) return;
+  }
+  
+  antest_get_current_cpu_info()->isLocked = true;
+  
+  // run the next thing!
+  anscheduler_loop_resign();
 }
