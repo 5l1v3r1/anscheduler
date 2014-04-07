e9d733e03c7db08eba3fcb6aacb6e1bd6b6dd370
Alex Nichol <unixpickle@gmail.com>
2014-04-07 17:34:09 -0400

    revised anscheduler_loop_run() call

--- src/loop.c
+++ src/loop.c
@@ -86,6 +86,13 @@ void anscheduler_loop_push(thread_t * thread) {
 }
 
 void anscheduler_loop_run() {
+  task_t * task = anscheduler_cpu_get_task();
+  anscheduler_cpu_set_task(NULL);
+  anscheduler_cpu_set_thread(NULL);
+  if (task) {
+    anscheduler_task_dereference(task);
+  }
+  
   uint64_t timeout = 0;
   thread_t * thread = _next_thread(&timeout);
   anscheduler_timer_set(timeout);
